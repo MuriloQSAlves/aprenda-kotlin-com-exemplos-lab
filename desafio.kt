36ff0fe2f4ced5a84016f62267cd7eb53b146ac6
Murilo Siqueira <142277557+MuriloQSAlves@users.noreply.github.com>
2024-02-14 08:32:54 -0300

    Commitado em 14-02

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,35 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val nivel: Nivel)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Usuario(var nome: String, var idade: Int, var função: String) {
+  constructor() : this("", 0, "")
+}
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+      println("Um novo aluno ${usuario.nome} foi matriculado.")
+      
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
 
+val Formacao.conteudosEducaicionaisNomes: String
+get() = conteudos.joinToString { it.nome }
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+  val formacao = Formacao("Formação Kotlin", listOf(ConteudoEducacional("Utilizando keyword Object", Nivel.AVANCADO), ConteudoEducacional("Explorando mapas", Nivel.INTERMEDIARIO), ConteudoEducacional("Conhecendo o Kotlin", Nivel.BASICO)))
+
+  println("Conteúdos do curso: ${formacao.conteudosEducaicionaisNomes}")
+
+  val mario = Usuario("Mário", 30, "Android developer")
+  .also{
+    formacao.inscritos.add(it)
+  }
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
